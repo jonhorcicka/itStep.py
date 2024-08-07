e48afcb069b790fa7db2d20924d3f1ee97b8f9f1
Jan Horčička <jonhorcicka@gmail.com>
2024-08-07 10:33:13 +0200

    first commit

---
+++ 08_srpen/5-8_01/test_teenager.py
@@ -0,0 +1,13 @@
+teenager = input('Zadej svůj věk: ')
+teenager = int(teenager)
+
+if teenager < 13:
+    print("Nejsi teenager")
+elif  teenager > 19:
+    print("Nejsi teenager")
+else:
+    print("Jsi teenager")
+
+
+print('Zadal jsi:', teenager)
+# print(type(teenager))
